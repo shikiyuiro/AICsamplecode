53d32d1d465d912bd6deaeeeeb8a5bd1df4a3e81
kyawa <93078797+shikiyuiro@users.noreply.github.com>
2023-10-20 19:25:37 +0900

    Update WaveletMatrix.cpp

--- WaveletMatrix.cpp
+++ WaveletMatrix.cpp
@@ -4,16 +4,12 @@ public:
     explicit WaveletMatrix() = default;
     WaveletMatrix(vector<i64> &_v){ internal_WaveletMatrix(_v);};
     i64 range_freq(i64 L, i64 R, i64 lower, i64 upper){ return internal_range_freq(L, R, upper) - internal_range_freq(L, R, lower);}
-    i64 range_Kthmin(i64 L, i64 R, i64 K){ return internal_range_Kthmin_assignXOR(L, R, K, 0);}
-    i64 range_Kthmax(i64 L, i64 R, i64 K){ return internal_range_Kthmin_assignXOR(L, R, R - L - K - 1, 0);}
-    i64 range_successor(i64 L, i64 R, i64 value){ i64 C = internal_range_freq(L, R, value + 1); return C == R - L ? -1 : internal_range_Kthmin_assignXOR(L, R, C, 0);}
-    i64 range_predecessor(i64 L, i64 R, i64 value){ i64 C = internal_range_freq(L, R, value); return C == 0 ? -1 : internal_range_Kthmin_assignXOR(L, R, C - 1, 0);}
-    i64 range_min_assignXOR(i64 L, i64 R, i64 value){ return internal_range_Kthmin_assignXOR(L, R, 0, value);}
-    i64 range_max_assignXOR(i64 L, i64 R, i64 value){ return internal_range_Kthmin_assignXOR(L, R, R - L - 1, value);}
-    i64 range_Kthmin_assignXOR(i64 L, i64 R, i64 K, i64 value){ return internal_range_Kthmin_assignXOR(L, R, K, value);}
-    i64 range_Kthmax_assignXOR(i64 L, i64 R, i64 K, i64 value){ return internal_range_Kthmin_assignXOR(L, R, R - L - K - 1, value);}
+    i64 range_Kthmin(i64 L, i64 R, i64 K){ return internal_range_Kthmin(L, R, K);}
+    i64 range_Kthmax(i64 L, i64 R, i64 K){ return internal_range_Kthmin(L, R, R - L - K - 1);}
+    i64 range_successor(i64 L, i64 R, i64 value){ i64 C = internal_range_freq(L, R, value + 1); return C == R - L ? -1 : internal_range_Kthmin(L, R, C);}
+    i64 range_predecessor(i64 L, i64 R, i64 value){ i64 C = internal_range_freq(L, R, value); return C == 0 ? -1 : internal_range_Kthmin(L, R, C - 1);}
 private:
-    vector<i64> Matrix;
+    vector<vector<i64>> Matrix;
     i64 bitsize;
     i64 prefixsize;
     void internal_WaveletMatrix(vector<i64> v){
@@ -21,12 +17,12 @@ private:
         i64 v_max = *max_element(v.begin(), v.end()) + 1;
         bitsize = 64 - __builtin_clzll(v_max);
         prefixsize = v.size()+1;
-        Matrix.resize(prefixsize * bitsize);
+        Matrix.resize(bitsize, vector<i64>(prefixsize, 0));
         for(i64 h = bitsize - 1; h >= 0; h--){
             for(i64 i = 0; i < v.size(); i++){
-                Matrix[i+1 + prefixsize * h] = Matrix[i + prefixsize * h] + (v[i] >> h & 1);
+                Matrix[h][i+1] = Matrix[h][i] + (v[i] >> h & 1);
             }
-            stable_sort(v.begin(), v.end(), [&](auto a, auto b){ return (a >> h & 1) < (b >> h & 1);});
+            stable_sort(v.begin(), v.end(), [&](auto a, auto b){ return (a >> h & 1) > (b >> h & 1);});
         }
     };
     i64 internal_range_freq(i64 L, i64 R, i64 upper){
@@ -34,7 +30,7 @@ private:
         if(upper < 0) return 0;
         i64 res = 0;
         for(i64 h = bitsize - 1; h >= 0; h--){
-            auto [L0, R0, L1, R1] = decomp(L, R, h);
+            auto [L1, R1, L0, R0] = decomp(L, R, h);
             if(upper & ((i64)1 << h)){
                 res += R0 - L0;
                 tie(L, R) = {L1, R1};
@@ -44,13 +40,10 @@ private:
         }
         return res;
     };
-    i64 internal_range_Kthmin_assignXOR(i64 L, i64 R, i64 K, i64 value){
+    i64 internal_range_Kthmin(i64 L, i64 R, i64 K){
         i64 res = 0;
         for(i64 h = bitsize - 1; h >= 0; h--){
-            auto [L0, R0, L1, R1] = decomp(L, R, h);
-            if(value & ((i64)1 << h)){
-                swap(L0, L1); swap(R0, R1);
-            }
+            auto [L1, R1, L0, R0] = decomp(L, R, h);
             if(K < R0 - L0){
                 tie(L, R) = {L0, R0};
             }else{
@@ -63,11 +56,11 @@ private:
     }
     tuple<i64,i64,i64,i64> decomp(i64 L, i64 R, i64 h){
         return tuple<i64,i64,i64,i64>({
-            L - Matrix[L + prefixsize * h],
-            R - Matrix[R + prefixsize * h],
-            prefixsize - 1 - Matrix[prefixsize * (h+1) - 1] + Matrix[L + prefixsize * h],
-            prefixsize - 1 - Matrix[prefixsize * (h+1) - 1] + Matrix[R + prefixsize * h]
+            Matrix[h][L],
+            Matrix[h][R],
+            Matrix[h].back() + (L - Matrix[h][L]),
+            Matrix[h].back() + (R - Matrix[h][R])
         });
     }
-    //https://judge.yosupo.jp/submission/166929
+    //https://judge.yosupo.jp/submission/167370
 };
